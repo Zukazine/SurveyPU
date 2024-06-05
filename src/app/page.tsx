561994bfad082f8483c8cb419ecd7fec4402ab20
Zukazine <zukazine007@gmail.com>
2024-06-05 15:52:56 +0700

    clear non necessary import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 'use client'
 
 import React from 'react';
-import MapDraw from './_components/mapdraw';
-import FormWithPersistence from './_components/persistanceForm';
 import MainSurvey from './_components/mainSurvey';
 
 export default function Home() {
   return (
     <>
-      {/* <FormWithPersistence />  */}
-      {/* <MapDraw /> */}
       <MainSurvey />
     </>
   );
